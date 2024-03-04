c012f8291b05ec74602e71a32aa16be6f18aeb60
vitormpp <vitormanuelpp@gmail.com>
2024-03-04 03:02:22 +0000

    Implemented utility functions

--- lab2/utils.c
+++ lab2/utils.c
@@ -3,22 +3,18 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  *lsb = (u_int8_t) (val & 0x00FF);
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  *msb = (u_int8_t) ((val & 0xFF00) >> 8);
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  uint32_t tmp;
+  int ret = sys_inb(port, &tmp);
+  *value = (uint8_t) tmp;
+  return ret;
 }
