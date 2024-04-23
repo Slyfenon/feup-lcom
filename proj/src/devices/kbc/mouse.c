73bc63d6437d5c0a53d6a65b143d04a0c6f35315
g0ncal0 <goncalonunes1112@gmail.com>
2024-04-23 17:01:32 +0100

    Added error messages in mouse functions

--- proj/src/devices/kbc/mouse.c
+++ proj/src/devices/kbc/mouse.c
@@ -12,21 +12,27 @@ extern uint8_t stat;
 
 int (mouse_subscribe_int)(uint8_t *bit_no) {
     *bit_no = mouse_hook_id;
-    if (sys_irqsetpolicy(MOUSE_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &mouse_hook_id) != 0) return 0;
+    if (sys_irqsetpolicy(MOUSE_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &mouse_hook_id) != OK) {
+        printf("sys_irqsetpolicy failed inside %s\n", __func__);
+        return EXIT_FAILURE;
+    }    
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 int (mouse_unsubscribe_int)() {
-    if (sys_irqrmpolicy(&mouse_hook_id) != 0) return 1;
+    if (sys_irqrmpolicy(&mouse_hook_id) != 0) {
+        printf("sys_irqrmpolicy failed inside %s\n", __func__);
+        return EXIT_FAILURE;
+    }    
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 void (mouse_ih)() {
     uint8_t byte;
 
-    if (util_sys_inb(KBC_OUT_BUF, &byte) != 0) indexArray = 0;
+    if (util_sys_inb(KBC_OUT_BUF, &byte) != OK) indexArray = 0;
 
     else if (stat & (KBC_PAR_ERR | KBC_TO_ERR)) indexArray = 0;
 
@@ -44,18 +50,25 @@ int (kbc_write_command)(uint8_t cmd) {
     int tries = MAX_TRIES;
 
     while (tries) {
-        if (util_sys_inb(KBC_CMD_REG, &stat) != 0) return 1;
+        if (util_sys_inb(KBC_CMD_REG, &stat) != OK) {
+            printf("util_sys_inb failed inside %s\n", __func__);
+            return EXIT_FAILURE;
+        }
 
-        if ((stat & KBC_IBF) == 0) {
-            if (sys_outb(KBC_CMD_REG, cmd) != 0) return 1;
-            return 0;
+        if ((stat & KBC_IBF) == OK) {
+            if (sys_outb(KBC_CMD_REG, cmd) != OK) {
+                printf("sys_outb failed inside %s\n", __func__);
+                return EXIT_FAILURE;
+            }
+            return EXIT_SUCCESS;
         }
 
         tries--;
         tickdelay(micros_to_ticks(DELAY));
     }
 
-    return 1;
+    printf("%s failed all tries\n", __func__);
+    return EXIT_FAILURE;
 }
 
 int (write_byte_to_mouse)(uint8_t byte) {
@@ -63,35 +76,48 @@ int (write_byte_to_mouse)(uint8_t byte) {
     uint8_t ack;
 
     while (tries) {
-        if (kbc_write_command(WRITE_BYTE_MOUSE) != 0) return 1;
+        if (kbc_write_command(WRITE_BYTE_MOUSE) != OK) {
+            printf("kbc_write_command failed inside %s", __func__);
+            return EXIT_FAILURE;
+        }
 
         tickdelay(micros_to_ticks(DELAY));
 
-        if (util_sys_inb(KBC_CMD_REG, &stat) != 0) return 1;
+        if (util_sys_inb(KBC_CMD_REG, &stat) != OK) {
+            printf("util_sys_inb failed inside %s", __func__);
+            return EXIT_FAILURE;
+        }
 
-        if ((stat & KBC_IBF) == 0) {
-            if (sys_outb(KBC_OUT_BUF, byte) != 0) return 1;
+        if ((stat & KBC_IBF) == OK) {
+            if (sys_outb(KBC_OUT_BUF, byte) != OK) {
+                printf("sys_outb failed inside %s", __func__);
+                return EXIT_FAILURE;
+            }
 
             tickdelay(micros_to_ticks(DELAY));
 
-            if (util_sys_inb(KBC_OUT_BUF, &ack) != 0) return 1;
+            if (util_sys_inb(KBC_OUT_BUF, &ack) != OK) {
+                printf("util_sys_inb failed inside %s", __func__);
+                return EXIT_FAILURE;
+            }
 
-            if (ack == ACK) return 0;
+            if (ack == ACK) return EXIT_SUCCESS;
         }
 
         tries--;
         tickdelay(micros_to_ticks(DELAY));
     }
 
-    return 1;
+    printf("%s failed all tries\n", __func__);
+    return EXIT_FAILURE;
 }
 
 int (mouse_enable_data_reporting_mine)() {
     int tries = MAX_TRIES;
 
     while (tries) {
-        if (write_byte_to_mouse(MOUSE_ENABLE_DATA_REPORT) == 0) {
-            return 0;
+        if (write_byte_to_mouse(MOUSE_ENABLE_DATA_REPORT) == OK) {
+            return EXIT_SUCCESS;
         }
 
         else {
@@ -100,15 +126,16 @@ int (mouse_enable_data_reporting_mine)() {
         }
     }
 
-    return 0;
+    printf("%s failed all tries\n", __func__);
+    return EXIT_FAILURE;
 }
 
 int (mouse_disable_data_reporting)() {
     int tries = MAX_TRIES;
 
     while (tries) {
-        if (write_byte_to_mouse(MOUSE_DISABLE_DATA_REPORT) == 0) {
-            return 0;
+        if (write_byte_to_mouse(MOUSE_DISABLE_DATA_REPORT) == OK) {
+            return EXIT_SUCCESS;
         }
 
         else {
@@ -117,20 +144,8 @@ int (mouse_disable_data_reporting)() {
         }
     }
 
-    return 0;
-}
-
-void (mouse_parse_packet)(struct packet *pp) {
-    pp->bytes[0] = bytes[0];
-    pp->bytes[1] = bytes[1];
-    pp->bytes[2] = bytes[2];
-    pp->rb = bytes[0] & RIGHT_BUTTON;
-    pp->mb = bytes[0] & MIDDLE_BUTTON;
-    pp->lb = bytes[0] & LEFT_BUTTON;
-    pp->delta_x = (bytes[0] & MSB_X_DELTA) ? (0xFF00 | bytes[1]) : bytes[1];
-    pp->delta_y = (bytes[0] & MSB_Y_DELTA) ? (0xFF00 | bytes[2]) : bytes[2];
-    pp->x_ov = bytes[0] & X_OVF;
-    pp->y_ov = bytes[0] & Y_OVF;
+    printf("%s failed all tries\n", __func__);
+    return EXIT_FAILURE;
 }
 
 int (mouse_set_sample_rate)(uint8_t rate) {
@@ -139,14 +154,15 @@ int (mouse_set_sample_rate)(uint8_t rate) {
     while (tries) {
         tries--;
 
-        if (write_byte_to_mouse(MOUSE_SET_SAMPLE_RATE) != 0) continue;;
+        if (write_byte_to_mouse(MOUSE_SET_SAMPLE_RATE) != 0) continue;
 
         tickdelay(micros_to_ticks(DELAY));
 
-        if (write_byte_to_mouse(rate) == 0) return 0;
+        if (write_byte_to_mouse(rate) == OK) return EXIT_SUCCESS;
     }
 
-    return 1;
+    printf("%s failed all tries\n", __func__);
+    return EXIT_FAILURE;
 }
 
 // can't use the generic write_byte_to_mouse because the output (device id) overwrites the ack
@@ -154,25 +170,36 @@ int (write_get_device_id_to_mouse)(uint8_t* id) {
     int tries = MAX_TRIES;
 
     while (tries) {
-        if (kbc_write_command(WRITE_BYTE_MOUSE) != 0) return 1;
+        if (kbc_write_command(WRITE_BYTE_MOUSE) != OK) {
+            printf("kbc_write_command failed inside %s\n", __func__);
+            return EXIT_FAILURE;
+        }
 
         tickdelay(micros_to_ticks(DELAY));
 
-        if (util_sys_inb(KBC_CMD_REG, &stat) != 0) return 1;
+        if (util_sys_inb(KBC_CMD_REG, &stat) != OK) {
+            printf("util_sys_inb failed inside %s\n", __func__);
+            return EXIT_FAILURE;
+        }
 
-        if ((stat & KBC_IBF) == 0) {
-            if (sys_outb(KBC_OUT_BUF, MOUSE_GET_DEVICE_ID) != 0) return 1;
+
+        if ((stat & KBC_IBF) == OK) {
+            if (sys_outb(KBC_OUT_BUF, MOUSE_GET_DEVICE_ID) != OK) {
+                printf("sys_outb failed inside %s\n", __func__);
+                return EXIT_FAILURE;
+            }
 
             tickdelay(micros_to_ticks(DELAY));
 
-            if (util_sys_inb(KBC_OUT_BUF, id) == 0) return 0;
+            if (util_sys_inb(KBC_OUT_BUF, id) == OK) return EXIT_SUCCESS;
         }
 
         tries--;
         tickdelay(micros_to_ticks(DELAY));
     }
 
-    return 1;
+    printf("%s failed all tries\n", __func__);
+    return EXIT_FAILURE;
 }
 
 int (get_mouse_id)(uint8_t* id) {
@@ -181,7 +208,7 @@ int (get_mouse_id)(uint8_t* id) {
     while (tries) {
         tries--;
 
-        if (write_get_device_id_to_mouse(id) != 0) continue;
+        if (write_get_device_id_to_mouse(id) != OK) continue;
 
         tickdelay(micros_to_ticks(DELAY));
 
@@ -189,26 +216,41 @@ int (get_mouse_id)(uint8_t* id) {
         break;
     }
 
-    if (!tries) return 1;
+    if (!tries) {
+        printf("%s failed all tries\n", __func__);
+        return EXIT_FAILURE;
+    }    
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 int (enable_scrolling)(){
     uint8_t id = 0;
 
-    if (mouse_set_sample_rate(200) != 0) return 1;
-
-    if (mouse_set_sample_rate(100) != 0) return 1;
-
-    if (mouse_set_sample_rate(80) != 0) return 1;
-
-    if (get_mouse_id(&id) != 0) return 1;
-
-    if (id != INTELLIMOUSE) {
-        printf("Failed to enable scrolling\n");
-        return 1;
+    if (mouse_set_sample_rate(200) != OK) {
+        printf("mouse_set_sample_rate failed inside %s\n", __func__);
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    if (mouse_set_sample_rate(100) != OK) {
+        printf("mouse_set_sample_rate failed inside %s\n", __func__);
+        return EXIT_FAILURE;
+    }
+
+    if (mouse_set_sample_rate(80) != OK) {
+        printf("mouse_set_sample_rate failed inside %s\n", __func__);
+        return EXIT_FAILURE;
+    }
+
+    if (get_mouse_id(&id) != OK) {
+        printf("get_mouse_id failed inside %s\n", __func__);
+        return EXIT_FAILURE;
+    }
+
+    if (id != INTELLIMOUSE) {
+        printf("%s failed\n", __func__);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
