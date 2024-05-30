1cd7b2d29e0405ebcccc7522dc3b61461575ea8d
Nuno Machado <saavedra.machado.1@gmail.com>
2024-05-30 16:19:21 +0100

    Added small documentation

--- proj/src/game/dynamite.h
+++ proj/src/game/dynamite.h
@@ -8,13 +8,31 @@
 #define DYNAMITE_Y 109
 
 
+/**
+ * @struct Dynamite
+ * @brief Represents a dynamite object in the game.
+ *
+ * The Dynamite struct contains information about the position, activity status,
+ * direction, and explosion frame of a dynamite object.
+ */
 typedef struct {
-    Position pos;
-    bool active;
-    Direction dir;
-    int explosionFrame;
+    Position pos;           /**< The position of the dynamite */
+    bool active;            /**< Indicates if the dynamite is active */
+    Direction dir;          /**< The direction of the dynamite */
+    int explosionFrame;     /**< The current explosion frame of the dynamite */
 } Dynamite;
 
+/**
+ * @brief Creates a Dynamite object with the specified coordinates and direction.
+ *
+ * This function creates a Dynamite object with the given x and y coordinates and sets its direction.
+ *
+ * @param x The x-coordinate of the Dynamite object.
+ * @param y The y-coordinate of the Dynamite object.
+ * @param dir The direction of the Dynamite object.
+ *
+ * @return A pointer to the created Dynamite object.
+ */
 Dynamite* createDynamite(int x, int y, Direction dir);
 
 #endif
--- proj/src/game/gameover.h
+++ proj/src/game/gameover.h
@@ -7,8 +7,25 @@
 Sprite *GameOver;
 Sprite *GameOverESC; 
 
+/**
+ * @brief Loads the game over screen.
+ * 
+ * This function is responsible for loading the game over screen, which is displayed when the game ends.
+ * It loads the individual elements of the screen, such as the game over text and the score.
+ * 
+ * @return void
+ */
 void load_gameover();
 
+/**
+ * Draws the game over score on the screen.
+ * This function is responsible for displaying the final score
+ * achieved by the player after the game is over.
+ */
 void draw_GameOverScore();
 
+/**
+ * @brief Draws the game over screen.
+ * Draws all the elements of the game over screen, such as the game over text and the score.
+*/
 void draw_gameover();
