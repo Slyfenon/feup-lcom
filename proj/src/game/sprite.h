49b319496c8530563abd4d467f3489fcdb3e4cc0
Nuno Machado <saavedra.machado.1@gmail.com>
2024-06-02 03:21:09 +0100

    Add Wait message

--- proj/src/game/sprite.h
+++ proj/src/game/sprite.h
@@ -5,11 +5,12 @@
 #include "../resources/font/font.xpm"
 #include "../resources/font/gameover.xpm"
 #include "../resources/font/gameoverEsc.xpm"
-#include "../resources/font/numbers.xpm"
-#include "../resources/font/singleplayer.xpm"
 #include "../resources/font/multiplayer.xpm"
+#include "../resources/font/numbers.xpm"
 #include "../resources/font/quit.xpm"
 #include "../resources/font/score.xpm"
+#include "../resources/font/singleplayer.xpm"
+#include "../resources/font/wait.xpm"
 #include "../resources/sprites/aim.xpm"
 #include "../resources/sprites/clock.xpm"
 #include "../resources/sprites/dayDesert.xpm"
@@ -26,33 +27,34 @@
  */
 typedef struct Sprite {
   uint16_t height; /** The height of the sprite. */
-  uint16_t width; /** The width of the sprite. */
-  uint32_t *map; /** The pixel map of the sprite. */
+  uint16_t width;  /** The width of the sprite. */
+  uint32_t *map;   /** The pixel map of the sprite. */
 } Sprite;
 
-Sprite *dayDesert; /** Pointer to the day desert sprite. */
-Sprite *nightDesert; /** Pointer to the night desert sprite. */
-Sprite *aim; /** Pointer to the aim sprite. */
-Sprite *target; /** Pointer to the target sprite. */
-Sprite *fall[3]; /** Array of pointers to the fall sprites. */
+Sprite *dayDesert;    /** Pointer to the day desert sprite. */
+Sprite *nightDesert;  /** Pointer to the night desert sprite. */
+Sprite *aim;          /** Pointer to the aim sprite. */
+Sprite *target;       /** Pointer to the target sprite. */
+Sprite *fall[3];      /** Array of pointers to the fall sprites. */
 Sprite *dynamiteIcon; /** Pointer to the dynamite icon sprite. */
 Sprite *explosion[5]; /** Array of pointers to the explosion sprites. */
-Sprite *clockIcon; /** Pointer to the clock icon sprite. */
-Sprite *scoreSprite; /** Pointer to the score sprite. */
-Sprite *numbers[10]; /** Array of pointers to the number sprites. */
+Sprite *clockIcon;    /** Pointer to the clock icon sprite. */
+Sprite *scoreSprite;  /** Pointer to the score sprite. */
+Sprite *numbers[10];  /** Array of pointers to the number sprites. */
 Sprite *singleplayer; /** Pointer to the singleplayer sprite. */
-Sprite *multiplayer; /** Pointer to the multiplayer sprite. */
-Sprite *quit; /** Pointer to the quit sprite. */
-Sprite *dots; /** Pointer to the dots sprite. */
-Sprite *GameOver; /** Pointer to the Game Over sprite. */
-Sprite *GameOverESC; /** Pointer to the Game Over ESC sprite. */
+Sprite *multiplayer;  /** Pointer to the multiplayer sprite. */
+Sprite *quit;         /** Pointer to the quit sprite. */
+Sprite *dots;         /** Pointer to the dots sprite. */
+Sprite *waitMessage;  /** Pointer to the wait Message sprite. */
+Sprite *GameOver;     /** Pointer to the Game Over sprite. */
+Sprite *GameOverESC;  /** Pointer to the Game Over ESC sprite. */
 
 /**
  * @brief Creates a sprite from an XPM map.
  * @param xpm The XPM map representing the sprite.
  * @return A pointer to the created sprite.
  */
-Sprite *(create_sprite)(xpm_map_t xpm);
+Sprite *(create_sprite) (xpm_map_t xpm);
 
 /**
  * @brief Draws a sprite on the screen.
@@ -61,28 +63,28 @@ Sprite *(create_sprite)(xpm_map_t xpm);
  * @param y The y-coordinate of the sprite.
  * @return 0 upon success, non-zero otherwise.
  */
-int (draw_sprite)(Sprite *sprite, int x, int y);
+int(draw_sprite)(Sprite *sprite, int x, int y);
 
 /**
  * @brief Loads the menu sprites.
  */
-void (loadMenu)();
+void(loadMenu)();
 
 /**
  * @brief Loads the game over sprites.
  */
-void (loadGameOver)();
+void(loadGameOver)();
 
 /**
  * @brief Loads all the sprites.
  * @return 0 upon success, non-zero otherwise.
  */
-int (load_sprites)();
+int(load_sprites)();
 
 /**
  * @brief Deletes all the loaded sprites.
  * @return 0 upon success, non-zero otherwise.
  */
-int (delete_sprites)();
+int(delete_sprites)();
 
 #endif
--- proj/src/game/wait_menu.c
+++ proj/src/game/wait_menu.c
@@ -7,5 +7,6 @@ void draw_wait_menu(bool isDay) {
   else {
     draw_background(nightDesert->map);
   }
+  draw_sprite(waitMessage, MAX_X / 2, MAX_Y / 2);
   vg_page_flipping();
 }
