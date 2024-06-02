253e2c9672ccd09740b6c8ad1235293806781990
vitormpp <vitormanuelpp@gmail.com>
2024-06-02 23:45:19 +0100

    Added documentation

--- proj/src/devices/timer/timer.c
+++ proj/src/devices/timer/timer.c
@@ -5,12 +5,12 @@
 
 #include "i8254.h"
 
-
 int counter = 0;
 int hook_id = TIMER0_IRQ;
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
-  *bit_no = hook_id;  return sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id);
+  *bit_no = hook_id;  
+  return sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id);
 }
 
 int (timer_unsubscribe_int)() {
